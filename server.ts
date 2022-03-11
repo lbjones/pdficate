607c71a0d9c70c3e95c915469a7cf4b13cca0649
Lynden Jones <lyndenjones@gmail.com>
2022-03-11 11:56:10 -0700

    improve logging

--- server.ts
+++ server.ts
@@ -8,6 +8,13 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+const logCopy = console.log.bind(console);
+console.log = function (data) {
+  var date = "[" + new Date().toUTCString() + "] ";
+  if (arguments?.length > 1) logCopy(date, arguments);
+  else logCopy(date, data);
+};
 interface PrintNode {
   path: string;
   updatedAt: string;
@@ -60,7 +67,7 @@ app.get("/clear-cache", (req, res) => {
   if (process.env.KEY !== key || !PrintEnvs.includes(env as PrintEnv) || !host)
     return res.sendStatus(404);
 
-  console.log("clearing cache: ", host, env);
+  console.log(`clearing cache:`, req.query);
 
   fs.rmSync(`./pdfs/${env}`, { recursive: true, force: true });
 
@@ -71,51 +78,56 @@ app.get("/clear-cache", (req, res) => {
 
 const generatePdfs = (host: string, env: PrintEnv) => {
   const listingUrl = `${host}/print-file-list.json`;
-  console.log("generatePdfs: ", listingUrl);
-  axios.get(listingUrl).then(async (response) => {
-    const browser = await puppeteer.launch();
-    for (const node of response.data) {
-      const printNode = node as PrintNode;
-      const filename = `./pdfs/${env}${node.path.slice(0, -1)}.pdf`;
-      if (
-        !fs.existsSync(filename) ||
-        fs.statSync(filename).birthtime < new Date(printNode.updatedAt)
-      ) {
-        const printUrl = `${host}${printNode.path}print/`;
-        console.log("refreshing: ", printUrl);
-        const html = await axios
-          .get(printUrl)
-          .then((response) => response.data)
-          .catch((err) => console.error("could not retrieve that last one"));
-        if (html) {
-          const page = await browser.newPage();
-          await page.setContent(html);
-          const dir = filename.substring(0, filename.lastIndexOf("/"));
-          if (!fs.existsSync(dir)) {
-            console.log(`dir ${dir} doesnt exist, creating it...`);
-            fs.mkdirSync(dir, { recursive: true });
+  console.log(`generatePdfs: ${listingUrl}`);
+  axios
+    .get(listingUrl)
+    .then(async (response) => {
+      const browser = await puppeteer.launch();
+      for (const node of response.data) {
+        const printNode = node as PrintNode;
+        const filename = `./pdfs/${env}${node.path.slice(0, -1)}.pdf`;
+        if (
+          !fs.existsSync(filename) ||
+          fs.statSync(filename).birthtime < new Date(printNode.updatedAt)
+        ) {
+          const printUrl = `${host}${printNode.path}print/`;
+          console.log(`refreshing: ${printUrl}`);
+          const html = await axios
+            .get(printUrl)
+            .then((response) => response.data)
+            .catch(() =>
+              console.log("Error...could not retrieve that last one")
+            );
+          if (html) {
+            const page = await browser.newPage();
+            await page.setContent(html);
+            const dir = filename.substring(0, filename.lastIndexOf("/"));
+            if (!fs.existsSync(dir)) {
+              console.log(`dir ${dir} doesnt exist, creating it...`);
+              fs.mkdirSync(dir, { recursive: true });
+            }
+            await page.pdf({
+              format: "letter",
+              path: filename,
+              margin: {
+                top: "32px",
+                right: "32px",
+                bottom: "32px",
+                left: "32px",
+              },
+              scale: 1,
+            });
           }
-          await page.pdf({
-            format: "letter",
-            path: filename,
-            margin: {
-              top: "32px",
-              right: "32px",
-              bottom: "32px",
-              left: "32px",
-            },
-            scale: 1,
-          });
         }
       }
-    }
-    await browser.close();
-  });
+      await browser.close();
+    })
+    .catch(() => console.log("Error...could not retrieve listing file"));
 };
 
 app.get("/get-pdf", async (req, res) => {
   const path = req.query.path;
-  console.log("getting: ", path);
+  console.log(`getting: ${path}`);
   if (!path || typeof path !== "string") return res.sendStatus(404);
 
   let env: PrintEnv = "production";
@@ -125,6 +137,6 @@ app.get("/get-pdf", async (req, res) => {
   const file = `./pdfs/${env}${path.slice(0, -1)}.pdf`;
   if (!fs.existsSync(file)) return res.sendStatus(404);
 
-  console.log("got: ", file);
+  console.log(`got: ${file}`);
   return res.download(file);
 });
